c566837429b963467081b1d99a8c495402f44899
Hugo Elias <hugo.elias@woozaventures.com.br>
2024-10-02 15:28:01 -0300

    Criação projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    component: HomeComponent,
+    data: { title: 'Planos odontológicos para você, família e empresa | W.Dental' }
+  },
+];
