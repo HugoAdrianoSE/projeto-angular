7c77b3d9a34ae219ff587d30ab1f792f358fa8f6
Hugo Elias <hugo.elias@woozaventures.com.br>
2024-10-03 16:11:30 -0300

    CoreUI

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { MenuComponent } from '../../components/menu/menu.component';
+import { CarouselModule } from '@coreui/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +11,36 @@ import { MenuComponent } from '../../components/menu/menu.component';
   styleUrl: './home.component.scss',
 
   imports: [
+    CarouselModule,
+    CommonModule,
+    MenuComponent,
     RouterLink,
     RouterModule,
-    MenuComponent,
   ],
 })
 
 export class HomeComponent implements OnInit {
+  public slides = [
+    {
+      title: 'Primeiro Slide',
+      subtitle: 'Este é o texto do primeiro slide',
+      src: '../../../assets/img/angular-banner1.webp',
+      active: true
+    },
+    {
+      title: 'Segundo Slide',
+      subtitle: 'Este é o texto do segundo slide',
+      src: '../../../assets/img/angular-banner2.webp',
+      active: false
+    },
+    {
+      title: 'Terceiro Slide',
+      subtitle: 'Este é o texto do terceiro slide',
+      src: '../../../assets/img/angular-banner3.webp',
+      active: false
+    }
+  ];
+
   constructor() { }
 
   public async ngOnInit() {
